1278fee1a60212b15d0aa4ae7f7afaa5507679ec
David J. Fiander <david@fiander.info>
2021-01-14 08:53:13 -0500

    Enable low power sleep on idle thread

--- keyboards/handwired/68keys/rules.mk
+++ keyboards/handwired/68keys/rules.mk
@@ -6,7 +6,7 @@ BOOTLOADER = halfkay
 BOARD = PJRC_TEENSY_3_1
 
 # Enter lower-power sleep mode when on the ChibiOS idle thread
-OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
+# OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
 
 # Build Options
 #   change yes to no to disable
