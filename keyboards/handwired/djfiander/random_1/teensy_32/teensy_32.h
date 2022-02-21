832d96752c98dd402aea0c5f57e8700551b2c3b8
David J. Fiander <david@fiander.info>
2022-02-21 14:57:02 -0500

    Initial teensy implementation. Clean compile, untested on hardware

---
+++ keyboards/handwired/djfiander/random_1/teensy_32/teensy_32.h
@@ -0,0 +1,34 @@
+/* Copyright 2019
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+// TODO: including this causes "error: expected identifier before '(' token" errors
+//#include "config_common.h"
+
+      /*
+       *  8,  9, 10, 11, 12, 14, 15, 16
+       * 17, 18, 19, 20, 21, 22, 23
+       */
+#define MATRIX_COL_PINS { \
+        D3, C3, C4, C6, C7, D1, C0, B0, \
+        B1, B3, B2, D5, D6, C1, C2 \
+    }
+
+                        /*  3,  4,  5,  6,  7*/
+#define MATRIX_ROW_PINS { A12, D7, D4, D2, D3  }
+
+#define LED_CAPS_LOCK_PIN D0    /* 2 */
