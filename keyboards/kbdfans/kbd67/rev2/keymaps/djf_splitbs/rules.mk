c9201c1dc32268cfd65d1257662621c5021f81bf
David J. Fiander <david@fiander.info>
2023-05-01 11:47:01 -0400

    Enable dynamic typing term to experiment with mod-tapping

--- keyboards/kbdfans/kbd67/rev2/keymaps/djf_splitbs/rules.mk
+++ keyboards/kbdfans/kbd67/rev2/keymaps/djf_splitbs/rules.mk
@@ -4,3 +4,4 @@ EXTRAKEY_ENABLE = no	       # Audio control and System control
 CONSOLE_ENABLE = no	       # Console for debugging
 BOOTMAGIC_ENABLE = no
 RGBLIGHT_ENABLE = yes		# Use it for capslock indicator
+DYNAMIC_TAPPING_TERM_ENABLE = yes
