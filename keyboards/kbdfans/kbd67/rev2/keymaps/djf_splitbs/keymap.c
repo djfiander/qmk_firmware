2353e5246ef912f4e1bf1176074167266087c471
David J. Fiander <david@fiander.info>
2023-05-07 09:29:23 -0400

    Switch from Hyper to MEH.

--- keyboards/kbdfans/kbd67/rev2/keymaps/djf_splitbs/keymap.c
+++ keyboards/kbdfans/kbd67/rev2/keymaps/djf_splitbs/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRAVE,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,  KC_INS,  \
   LCTL(KC_LALT), _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, _______, _______,          KC_DEL, KC_END, \
   _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,_______, _______,          _______,          _______, \
-  KC_HYPR,          _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,          KC_HYPR, _______, _______, \
+  KC_MEH,          _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,          KC_MEH, _______, _______, \
   _______, MO(_RL), _______,                   _______,                            _______, MO(_RL),        _______,  _______, _______, _______),
 
 [_RL] = LAYOUT_65_ansi_split_bs(
