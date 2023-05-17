3c7d52efad86a275a2b1a907609e98038c357d6a
David J. Fiander <david@fiander.info>
2023-05-17 15:58:03 -0400

    Standardize location of MEH key, and make it layer-tap with the APP key

--- keyboards/kbdfans/kbd67/rev2/keymaps/djf_splitbs/keymap.c
+++ keyboards/kbdfans/kbd67/rev2/keymaps/djf_splitbs/keymap.c
@@ -33,6 +33,8 @@
 #define HOME_L LALT_T(KC_L)
 #define HOME_SCLN RGUI_T(KC_SCLN)
 
+#define MEH_APP MEH_T(KC_APP)
+
 #define _BL 0
 #define _FL 1
 #define _RL 2
@@ -43,13 +45,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,        KC_BSPC, KC_HOME,
   CTL_CAPS,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,   KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,         KC_ENT,         KC_PGUP,
   KC_LSFT,      KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,      KC_RSFT,         KC_UP,   KC_PGDN,
-  MO(_FL), KC_LGUI,   KC_LALT,                   KC_SPC,                                  KC_RALT,   KC_RGUI, MO(_FL), KC_LEFT, KC_DOWN, KC_RGHT),
+  MO(_FL), KC_LGUI,   KC_LALT,                   KC_SPC,                                  KC_RALT,   KC_RGUI, MEH_APP, KC_LEFT, KC_DOWN, KC_RGHT),
 
 [_FL] = LAYOUT_65_ansi_split_bs(
   KC_GRAVE,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8, KC_F9, KC_F10,  KC_F11,  KC_F12, _______, _______,    KC_INS,
   LCTL(KC_LALT), _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, _______, _______,    KC_DEL,  KC_END,
   _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,_______, _______,            _______,        _______,
-  KC_MEH,          _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,   KC_MEH,          _______, _______,
+  _______,          _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,   _______,          _______, _______,
   _______, MO(_RL), _______,                   _______,                            _______, MO(_RL),        _______,   _______, _______, _______),
 
 [_RL] = LAYOUT_65_ansi_split_bs(
