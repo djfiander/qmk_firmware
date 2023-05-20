67de3295f2d2f5e3e6b85850fe50a16fa68c45c1
David J. Fiander <david@fiander.info>
2023-05-19 21:05:03 -0400

    Move capslock to layer-tap on the Fn keys. Capslock key is CTRL only

--- keyboards/kbdfans/kbd67/rev2/keymaps/djf_splitbs/keymap.c
+++ keyboards/kbdfans/kbd67/rev2/keymaps/djf_splitbs/keymap.c
@@ -34,6 +34,7 @@
 #define HOME_SCLN RGUI_T(KC_SCLN)
 
 #define MEH_APP MEH_T(KC_APP)
+#define FL_CAPS LT(1, KC_CAPS)
 
 #define _BL 0
 #define _FL 1
@@ -43,9 +44,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BL] = LAYOUT_65_ansi_split_bs(
   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,  KC_DEL,
   KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,        KC_BSPC, KC_HOME,
-  CTL_CAPS,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,   KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,         KC_ENT,         KC_PGUP,
+  KC_LCTL,     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,   KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,         KC_ENT,         KC_PGUP,
   KC_LSFT,      KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,      KC_RSFT,         KC_UP,   KC_PGDN,
-  MO(_FL), KC_LGUI,   KC_LALT,                   KC_SPC,                                  KC_RALT,   KC_RGUI, MEH_APP, KC_LEFT, KC_DOWN, KC_RGHT),
+  FL_CAPS, KC_LGUI,   KC_LALT,                   KC_SPC,                                  KC_RALT,   KC_RGUI, MEH_APP, KC_LEFT, KC_DOWN, KC_RGHT),
 
 [_FL] = LAYOUT_65_ansi_split_bs(
   KC_GRAVE,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8, KC_F9, KC_F10,  KC_F11,  KC_F12, _______, _______,    KC_INS,
